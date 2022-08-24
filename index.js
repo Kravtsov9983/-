067745a6e13582e27e3e78acee81a8528b18e83b
Kravtsov <kravtsovofficial99@gmail.com>
2022-08-24 15:26:24 +0300

    work

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const film = document.querySelector(".midle")
+$('.back-to-top').click(function () {
+    $('body,html').animate({ scrollTop: 0}, 800);
+});
+
+$(window).scroll(function() {
+    let scrolled = $(window).scrollTop();
+
+    if(scrolled > 350) {
+        $('.back-to-top').addClass('active');
+    } else {
+        $('.back-to-top').removeClass('active');
+    }
+});
--- srs/modal.js
+++ srs/modal.js
@@ -9,4 +9,4 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
-  }
+}
